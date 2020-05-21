27890cb75ad0988b44057722facaa1f6858b0d69
hamayungill <hamayunsafdar1947@gmail.com>
2020-05-21 17:11:39 +0500

    deplou

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 // 'mongodb+srv://test:test@cluster0-pkpol.mongodb.net/test?retryWrites=true&w=majority'mongodb+srv://test:<password>@cluster0-pkpol.mongodb.net/test?retryWrites=true&w=majority
-mongoose.connect("mongodb+srv://test:test@cluster0-pkpol.mongodb.net/test?retryWrites=true&w=majority", {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
 }).then(
-  console.log('Mongodb Connected')
+  console.log('Mongodb Connected', process.env.MONGODB_URI)
 )
